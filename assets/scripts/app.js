3f99243cb8c97c80e46cc7ac7125869f62345822
Jailson P. Lima <jailsonp.lima03@gmail.com>
2023-06-14 16:43:25 -0300

    feat: add header and navbar
    add functional navbar only for mobile because of mobile first

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -1,1 +1,6 @@
-console.log("start");
+const menu = document.querySelector(".m-menu");
+const nav = document.querySelector(".m-nav");
+
+menu.addEventListener("click", () => {
+  nav.classList.toggle("m-nav--active");
+});
